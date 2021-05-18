3c186d3cd86cb8c54e4639af43791289ff2e1c44
Thanush Dinesh <thanush38@outlook.com>
2021-05-18 07:29:28 -0400

    sending https request

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import * as ELEMENTS from './elements.js';
-import {Http} from 'http.js';
-
+import {Http} from './http.js';
+const APP_ID = '4d1032d13b26dd5bd552092a7b5a8f93'
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
 function searchWeather() {
@@ -9,5 +9,11 @@ function searchWeather() {
         return alert('please enter a city name');
     }
     alert(CITY_NAME);
-    const URL
+    const URL = 'https://api.openweathermap.org/data/2.5/weather?q=' + CITY_NAME + '&units=metric&appid=' + APP_ID;
+
+    http.fetchData(URL)
+    .then(responseData => {
+
+    })
+    .catch(error => alert('error'));
 };
